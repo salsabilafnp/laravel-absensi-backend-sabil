7342782a3890deb7f29ec68a92c286cbe232ee5f
salsabilafnp <salsabilafebriantynp@gmail.com>
2024-04-29 14:59:48 +0700

    auth with fortify

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.auth.auth-login');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', function () {
+        return view('pages.dashboard', ['type_menu' => 'home']);
+    })->name('home');
 });
